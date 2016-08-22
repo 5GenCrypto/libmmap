225a14f55b5e13882aca2055ea040cb1d6612e8b
Alex J. Malozemoff <amaloz@galois.com>
2016-08-22 10:09:12 -0700

    adjust mmap_clt.c
    Instead of having all the function interfaces at the top of the file, define the
    structs "inline".  Avoids repetition of function interfaces.

--- mmap/mmap_clt.c
+++ mmap/mmap_clt.c
@@ -3,66 +3,6 @@
 
 #include <gmp.h>
 
-static void clt_pp_clear_wrapper (mmap_pp *pp);
-static void clt_pp_read_wrapper  (mmap_pp *const pp, FILE *const fp);
-static void clt_pp_save_wrapper  (const mmap_pp *const pp, FILE *const fp);
-
-static const mmap_pp_vtable clt_pp_vtable =
-  { .clear  = clt_pp_clear_wrapper
-  , .fread  = clt_pp_read_wrapper
-  , .fwrite = clt_pp_save_wrapper
-  , .size   = sizeof(mmap_pp)
-  };
-
-static void clt_state_init_wrapper  (mmap_sk *const sk, size_t lambda, size_t kappa, size_t gamma, unsigned long ncores, aes_randstate_t randstate, bool verbose);
-static void clt_state_clear_wrapper (mmap_sk *const sk);
-static void clt_state_read_wrapper  (mmap_sk *const sk, FILE *const fp);
-static void clt_state_save_wrapper  (const mmap_sk *const sk, FILE *const fp);
-static void clt_state_get_modulus   (const mmap_sk *const sk, fmpz_t p_out);
-static const mmap_pp * clt_pp_init_wrapper (const mmap_sk *const sk);
-
-static const mmap_sk_vtable clt_sk_vtable =
-  { .init   = clt_state_init_wrapper
-  , .clear  = clt_state_clear_wrapper
-  , .fread  = clt_state_read_wrapper
-  , .fwrite = clt_state_save_wrapper
-  , .pp     = clt_pp_init_wrapper
-  , .size   = sizeof(clt_state)
-  , .plaintext_field = clt_state_get_modulus
-  };
-
-static void clt_enc_init_wrapper    (mmap_enc *const enc, const mmap_pp *const pp);
-static void clt_enc_clear_wrapper   (mmap_enc *const enc);
-static void clt_enc_fread_wrapper   (mmap_enc *const enc, FILE *const fp);
-static void clt_enc_fwrite_wrapper  (const mmap_enc *const enc, FILE *const fp);
-static void clt_enc_set_wrapper     (mmap_enc *const dest, const mmap_enc *const src);
-static void clt_enc_add_wrapper     (mmap_enc *const dest, const mmap_pp *const pp, const mmap_enc *const a, const mmap_enc *const b);
-static void clt_enc_mul_wrapper     (mmap_enc *const dest, const mmap_pp *const pp, const mmap_enc *const a, const mmap_enc *const b);
-static bool clt_enc_is_zero_wrapper (const mmap_enc *const enc, const mmap_pp *const pp);
-static void clt_encode_wrapper (mmap_enc *const enc, const mmap_sk *const sk, int n, const fmpz_t *plaintext, int *group, aes_randstate_t rng);
-
-static const mmap_enc_vtable clt_enc_vtable =
-  { .init    = clt_enc_init_wrapper
-  , .clear   = clt_enc_clear_wrapper
-  , .fread   = clt_enc_fread_wrapper
-  , .fwrite  = clt_enc_fwrite_wrapper
-  , .set     = clt_enc_set_wrapper
-  , .add     = clt_enc_add_wrapper
-  , .mul     = clt_enc_mul_wrapper
-  , .is_zero = clt_enc_is_zero_wrapper
-  , .encode  = clt_encode_wrapper
-  , .size    = sizeof(mmap_enc)
-  };
-
-const mmap_vtable clt_vtable =
-  { .pp  = &clt_pp_vtable
-  , .sk  = &clt_sk_vtable
-  , .enc = &clt_enc_vtable
-  };
-
-////////////////////////////////////////////////////////////////////////////////
-// implementation
-
 static void clt_pp_clear_wrapper (mmap_pp *pp)
 {
     clt_pp_clear(&(pp->clt_self));
@@ -77,6 +17,13 @@ static void clt_pp_save_wrapper (const mmap_pp *const pp, FILE *const fp)
 {
     clt_pp_fsave(fp, &(pp->clt_self));
 }
+
+static const mmap_pp_vtable clt_pp_vtable =
+  { .clear  = clt_pp_clear_wrapper
+  , .fread  = clt_pp_read_wrapper
+  , .fwrite = clt_pp_save_wrapper
+  , .size   = sizeof(mmap_pp)
+  };
 
 static void clt_state_init_wrapper (mmap_sk *const sk, size_t lambda, size_t kappa,
                                     size_t gamma, unsigned long ncores,
@@ -119,6 +66,16 @@ static const mmap_pp * clt_pp_init_wrapper (const mmap_sk *const sk)
     clt_pp_init(&(pp->clt_self), &(sk->clt_self));
     return pp;
 }
+
+static const mmap_sk_vtable clt_sk_vtable =
+  { .init   = clt_state_init_wrapper
+  , .clear  = clt_state_clear_wrapper
+  , .fread  = clt_state_read_wrapper
+  , .fwrite = clt_state_save_wrapper
+  , .pp     = clt_pp_init_wrapper
+  , .size   = sizeof(clt_state)
+  , .plaintext_field = clt_state_get_modulus
+  };
 
 static void clt_enc_init_wrapper (mmap_enc *const enc, const mmap_pp *const pp __attribute__ ((unused)))
 {
@@ -178,3 +135,22 @@ clt_encode_wrapper (mmap_enc *const enc, const mmap_sk *const sk, int n,
     }
     free(ins);
 }
+
+static const mmap_enc_vtable clt_enc_vtable =
+  { .init    = clt_enc_init_wrapper
+  , .clear   = clt_enc_clear_wrapper
+  , .fread   = clt_enc_fread_wrapper
+  , .fwrite  = clt_enc_fwrite_wrapper
+  , .set     = clt_enc_set_wrapper
+  , .add     = clt_enc_add_wrapper
+  , .mul     = clt_enc_mul_wrapper
+  , .is_zero = clt_enc_is_zero_wrapper
+  , .encode  = clt_encode_wrapper
+  , .size    = sizeof(mmap_enc)
+  };
+
+const mmap_vtable clt_vtable =
+  { .pp  = &clt_pp_vtable
+  , .sk  = &clt_sk_vtable
+  , .enc = &clt_enc_vtable
+  };
