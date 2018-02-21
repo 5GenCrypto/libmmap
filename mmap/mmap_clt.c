abf2f2ff65cee575632dd43e05466b9476c91cd9
Alex J. Malozemoff <amaloz@galois.com>
2018-02-21 10:01:25 -0800

    enable parallel encode

--- mmap/mmap_clt.c
+++ mmap/mmap_clt.c
@@ -36,7 +36,7 @@ clt_state_new_wrapper(const mmap_sk_params *params_,
     clt_state_t *sk;
     bool new_pows = false;
     int *pows;
-    int flags = CLT_FLAG_OPT_CRT_TREE | CLT_FLAG_OPT_COMPOSITE_PS;
+    int flags = CLT_FLAG_OPT_CRT_TREE | CLT_FLAG_OPT_COMPOSITE_PS | CLT_FLAG_OPT_PARALLEL_ENCODE;
     if (verbose)
         flags |= CLT_FLAG_VERBOSE;
 
